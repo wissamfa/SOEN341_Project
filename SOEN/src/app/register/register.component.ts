b28c6086c95632307900b051fd88747090e72d54
noamanfawaz <50502705+noamanfawaz@users.noreply.github.com>
2020-02-29 17:17:10 -0500

    #25-added the register functionality
    added the register functionality to the previously written front-end part of the code. 
    once the user clicks on the register button, his information is registered in the system.

--- SOEN/src/app/register/register.component.ts
+++ SOEN/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -18,14 +19,15 @@ export class RegisterComponent implements OnInit {
 
   constructor(
         private formBuilder: FormBuilder,
-        private router: Router
+        private router: Router,
+        private auth: AuthService
         ) {}
 
 
   // To validate that the user had filled up the info to register
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      username: ['', Validators.required],
+      username: [''],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required],
       userAge: ['', Validators.required],
@@ -37,10 +39,21 @@ export class RegisterComponent implements OnInit {
 
   // This function will call the validation to make sure all the fields are filled before sending it to the backend
   checkup() {
-    this.router.navigate(['/login']);
+    this.register_User();
   }
   // For sending the info of the user to the backend:
   register_User() {
+    const value = {
+      email: this.registerForm.get("email").value,
+      password: this.registerForm.get("password").value,
+    }
+    this.auth.doRegister(value).then(
+      res => {
+        this.router.navigate(['']);
+    },
+    err => {
+      console.error(err);
+    })
   }
 
 
@@ -58,6 +71,7 @@ export class RegisterComponent implements OnInit {
       READER.readAsDataURL(event.target.files[0]);
     }
   }
+
   private onSuccess() {
     this.selectedFile.pending = false;
     this.selectedFile.status = 'ok';
