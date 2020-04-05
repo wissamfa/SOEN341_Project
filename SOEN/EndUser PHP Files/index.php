9e328f4e00a95eb4eb519a2bdd0bc91a69b74737
Rami Ajam <ajam.rami99@gmail.com>
2020-04-04 21:39:29 -0400

    Organized the files, combined similar files together

--- SOEN/EndUser PHP Files/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// this redirects us directly to the main page when we run the localhost
-include("main.php");
-
-?>
--- SOEN/includes/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// connecting to the database
-$con = mysqli_connect("localhost","root","","social_network");
-?>
