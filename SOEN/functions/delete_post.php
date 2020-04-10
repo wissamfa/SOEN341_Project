36af2c6592e85f83f23eb085f221bc9f7361fc7a
wissam <wissam.fa@hotmail.com>
2020-04-10 14:47:10 -0400

    still testing

--- SOEN/functions/delete_post.php
+++ SOEN/functions/delete_post.php
@@ -4,10 +4,11 @@
 // allow us to connect to the database
 $con = mysqli_connect("localhost","root","","social_network") or die("Connection was not established");
 
-// get the post id and store it
+// get the post id and store it in a variable
 if(isset($_GET['post_id'])){
     $post_id = $_GET['post_id'];
 
+    // access the database and delete the
     $delete_post = "DELETE from posts where post_id ='$post_id'";
     $run_delete = mysqli_query($con, $delete_post);
 
